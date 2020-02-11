e58f1cac275abe4aebb8cdf1af2d07f81605f3e9
AlasdairSwan <aswan@edx.org>
2020-02-11 15:17:54 -0500

    Add state

--- src/App.js
+++ src/App.js
@@ -3,21 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Make the world better</h1>
-      </header>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: undefined,
+    };
+    this.setUser = this.setUser.bind(this);
+  }
+
+  setUser() {
+    const email = document.getElementById('user-email').value;
+
+    this.setState({
+      user: email,
+    });
+  }
+
+  getEmailCollectionForm() {
+    return (
       <div>
-        <form>
+        <form onSubmit={this.setUser}>
           <div className="form-group">
-            <label for="exampleInputEmail1">Email address</label>
+            <label htmlFor="user-email">Email address</label>
             <input
               type="email"
               className="form-control"
-              id="exampleInputEmail1"
+              id="user-email"
               aria-describedby="emailHelp"
               placeholder="Enter email"
             />
@@ -26,8 +39,25 @@ function App() {
           <button type="submit" className="btn btn-primary">Submit</button>
         </form>
       </div>
-    </div>
-  );
+    );
+  }
+
+  render() {
+    const { user } = this.state;
+
+    return (
+      <React.Fragment>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>Make the world better</h1>
+          </header>
+        </div>
+        {!user && this.getEmailCollectionForm()}
+        {user && `${user} is helping save the world`}
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
