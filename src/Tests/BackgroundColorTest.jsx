9c0d5ba9ebaf6fc53e87ad38df44ad1a3dcab7a9
AlasdairSwan <aswan@edx.org>
2020-02-12 13:56:09 -0500

    Update copy and set text style

--- src/Tests/BackgroundColorTest.jsx
+++ src/Tests/BackgroundColorTest.jsx
@@ -33,55 +33,52 @@ class BackgroundColorTest extends React.Component {
     const { background, foreground } = this.props;
 
     return (
-      <React.Fragment>
-        <div className="d-flex">
-          <div
-            className="sample w-50 border d-flex align-items-center justify-content-center"
-            style={{backgroundColor: background}}
-          >
-            <div style={{color: foreground}}>Words</div>
-          </div>
-
-          <div className="d-flex flex-column pl-4">
-            <button
-              className="btn btn-primary mb-2 text-left d-flex align-items-center"
-              onClick={() => this.submitFeedback(5) }
-            >
-              <FontAwesomeIcon icon={faGrinBeam} size="3x" />
-              <div className="btn-label ml-4">Strong</div>
-            </button>
-            <button
-              className="btn btn-primary mb-2 text-left d-flex align-items-center"
-              onClick={() => this.submitFeedback(4) }
-            >
-              <FontAwesomeIcon icon={faSmile} size="3x" />
-              <div className="btn-label ml-4">Comfortable</div>
-            </button>
-            <button
-              className="btn btn-primary mb-2 text-left d-flex align-items-center"
-              onClick={() => this.submitFeedback(3) }
-            >
-              <FontAwesomeIcon icon={faMeh} size="3x" />
-              <div className="btn-label ml-4">Readable with effort</div>
-            </button>
-            <button
-              className="btn btn-primary mb-2 text-left d-flex align-items-center"
-              onClick={() => this.submitFeedback(2) }
-            >
-              <FontAwesomeIcon icon={faFrown} size="3x" />
-              <div className="btn-label ml-4">Too much effort</div>
-            </button>
-            <button
-              className="btn btn-primary mb-2 text-left d-flex align-items-center"
-              onClick={() => this.submitFeedback(1) }
-            >
-              <FontAwesomeIcon icon={faAngry} size="3x" />
-              <div className="btn-label ml-4">Illegible</div>
-            </button>
-          </div>
+      <div className="d-flex test-module">
+        <div
+          className="sample w-50 border d-flex align-items-center justify-content-center"
+          style={{backgroundColor: background}}
+        >
+          <div className="test-text font-weight-normal" style={{color: foreground}}>How legible is this text?</div>
         </div>
 
-      </React.Fragment>
+        <div className="d-flex flex-column pl-4">
+          <button
+            className="btn btn-primary mb-2 text-left d-flex align-items-center"
+            onClick={() => this.submitFeedback(5) }
+          >
+            <FontAwesomeIcon icon={faGrinBeam} size="3x" />
+            <div className="btn-label ml-4">Strong</div>
+          </button>
+          <button
+            className="btn btn-primary mb-2 text-left d-flex align-items-center"
+            onClick={() => this.submitFeedback(4) }
+          >
+            <FontAwesomeIcon icon={faSmile} size="3x" />
+            <div className="btn-label ml-4">Comfortable</div>
+          </button>
+          <button
+            className="btn btn-primary mb-2 text-left d-flex align-items-center"
+            onClick={() => this.submitFeedback(3) }
+          >
+            <FontAwesomeIcon icon={faMeh} size="3x" />
+            <div className="btn-label ml-4">Readable with effort</div>
+          </button>
+          <button
+            className="btn btn-primary mb-2 text-left d-flex align-items-center"
+            onClick={() => this.submitFeedback(2) }
+          >
+            <FontAwesomeIcon icon={faFrown} size="3x" />
+            <div className="btn-label ml-4">Too much effort</div>
+          </button>
+          <button
+            className="btn btn-primary text-left d-flex align-items-center"
+            onClick={() => this.submitFeedback(1) }
+          >
+            <FontAwesomeIcon icon={faAngry} size="3x" />
+            <div className="btn-label ml-4">Illegible</div>
+          </button>
+        </div>
+      </div>
     );
   }
 }
