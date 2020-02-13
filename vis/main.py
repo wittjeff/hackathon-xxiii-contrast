ac27f725000c398a42070d268949714eb6e6a2e8
Rebecca Graber <rgraber@edx.org>
2020-02-13 15:19:50 -0500

    black and white

--- vis/main.py
+++ vis/main.py
@@ -4,6 +4,7 @@ import psycopg2 as pg
 import colorsys
 import collections
 import os
+from statistics import mean
 
 def connect_to_db():
     connection = pg.connect(host="ec2-184-72-235-159.compute-1.amazonaws.com",
@@ -19,10 +20,10 @@ cursor = connection.cursor()
 cursor.execute("Select background_red,background_green,background_blue, user_rating, foreground_red from results")
 black_data_points = []
 white_data_points = []
-score_by_text_color_by_background_color = collections.defaultdict(lambda: {'black': [], 'white': []})
+score_by_text_color_by_background_color = collections.defaultdict(lambda: {'black': [], 'white': [], 'result': 0.5})
 
 for row in cursor.fetchall():
-    hls = colorsys.rgb_to_hls(row[0],row[1],row[2])
+    hls = colorsys.rgb_to_hls(row[0]/255.0,row[1]/255.0,row[2]/255.0)
     black_text = row[4] == 0
     data_points = black_data_points if black_text else white_data_points
     data_points.append({'hue': hls[0], 'lum': hls[1], 'sat': hls[2], 'rating': row[3]})
@@ -42,10 +43,27 @@ sat_white = [obj['sat'] for obj in white_data_points]
 lum_white = [obj['lum'] for obj in white_data_points]
 rating_white = [obj['rating'] for obj in white_data_points]
 
+for hsl in score_by_text_color_by_background_color.keys():
+    results_dict = score_by_text_color_by_background_color[hsl]
+    if len(results_dict['black']) >= 1 and len(results_dict['white']) >= 1:
+        result = 0
+        if mean(results_dict['black']) < mean(results_dict['white']):
+            result = 1
+        score_by_text_color_by_background_color[hsl]['result'] = result
+
+
+filtered = dict(filter(lambda elem: elem[1]['result'] != 0.5,score_by_text_color_by_background_color.items()))
+
+all_hsl_tuples_as_lists = list(zip(*[(k[0],k[1],k[2],filtered[k]['result']) for k in filtered.keys()]))
+
+fig2 = go.Figure(data=go.Scatter3d(x=all_hsl_tuples_as_lists[0], y=all_hsl_tuples_as_lists[1], z=all_hsl_tuples_as_lists[2], mode='markers',
+                         marker=dict(size=5,color=all_hsl_tuples_as_lists[3],colorscale='Greys',)))
+fig2.update_layout(scene={'xaxis_title':'Hue','yaxis_title':'Saturation', 'zaxis_title':'Luminescence'}, showlegend=False)
+fig2.show()
 
 fig = make_subplots(rows=1, cols=2,
                     specs=[[{"type": "scene"}, {"type": "scene"}]],
-                    subplot_titles=("White Text", "Black Text"),
+                    subplot_titles=("Black Text", "White Text"),
                     horizontal_spacing=0.01)
 
 
